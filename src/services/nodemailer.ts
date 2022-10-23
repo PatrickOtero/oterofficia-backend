3abcb0fbb6be237ea99c5274af70abd9d79b6310
PATRICKOTERONOT\patri <patrick.rocha.otero@gmail.com>
2022-10-23 20:48:11 -0300

    configurações nodemailer alteradas

--- src/services/nodemailer.ts
+++ src/services/nodemailer.ts
@@ -2,12 +2,12 @@ const nodemailer = require('nodemailer')
 const handlebars = require('nodemailer-express-handlebars')
 
 const transporter = nodemailer.createTransport({
-  host: process.env.NODEMAILER_HOST || process.env.MAIL_SERVER_HOST,
-  port: process.env.NODEMAILER_PORT || process.env.MAIL_SERVER_PORT,
+  host: process.env.MAIL_SERVER_HOST || process.env.NODEMAILER_HOST,
+  port: process.env.MAIL_SERVER_PORT || process.env.NODEMAILER_PORT,
   secure: false, // true for 465, false for other ports
   auth: {
-    user: process.env.NODEMAILER_USER || process.env.MAIL_SERVER_USER,
-    pass: process.env.NODEMAILER_PASS || process.env.MAIL_SERVER_PASS,
+    user: process.env.MAIL_SERVER_USER || process.env.NODEMAILER_USER,
+    pass: process.env.MAIL_SERVER_PASS || process.env.NODEMAILER_PASS,
   },
 })
 
