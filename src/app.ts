904f98d96a4a9d9d772cd6fe7f96cd8685046d39
PATRICKOTERONOT\patri <patrick.rocha.otero@gmail.com>
2022-10-23 20:55:42 -0300

    Ainda tentando fazer a conexão cliente-servidor

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import dotenv from "dotenv"
 dotenv.config()
 
 import express from "express"
+import cors from "cors"
 import { routes } from "./routes"
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(routes)
 
 export { app }
