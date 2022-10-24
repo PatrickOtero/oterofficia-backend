35b0cb481ca48b093e65aade4b6ceeed1d73380f
PATRICKOTERONOT\patri <patrick.rocha.otero@gmail.com>
2022-10-23 21:04:00 -0300

    Ainda tentando fazer a conexão cliente-servidor 2

--- src/services/nodemailer.ts
+++ src/services/nodemailer.ts
@@ -2,12 +2,12 @@ const nodemailer = require('nodemailer')
 const handlebars = require('nodemailer-express-handlebars')
 
 const transporter = nodemailer.createTransport({
-  host: process.env.MAIL_SERVER_HOST || process.env.NODEMAILER_HOST,
+  host: process.env.NODEMAILER_HOST,
   port: 587,
   secure: false, // true for 465, false for other ports
   auth: {
-    user: process.env.MAIL_SERVER_USER || process.env.NODEMAILER_USER,
-    pass: process.env.MAIL_SERVER_PASS || process.env.NODEMAILER_PASS,
+    user: process.env.NODEMAILER_USER,
+    pass: process.env.NODEMAILER_PASS,
   },
 })
 
