a8a73dd5989bb8d1f31e57e7a46db05adb5ca110
PATRICKOTERONOT\patri <patrick.rocha.otero@gmail.com>
2022-10-23 22:35:35 -0300

    Ainda tentando fazer a conexão cliente-servidor 6

--- src/connection.ts
+++ src/connection.ts
@@ -1,11 +1,11 @@
 const knex = require('knex')({
     client: 'pg',
-    connection: {
-      user: process.env.DB_USER || process.env.DB_SERVER_USER,
+    connection: process.env.DB_SERVER_URL || {
+      user: process.env.DB_USER,
       port: 5432,
-      host: process.env.DB_HOST || process.env.DB_SERVER_HOST,
-      database: process.env.DB_DATABASE || process.env.DB_SERVER_DATABASE,
-      password: process.env.DB_PASS || process.env.DB_SERVER_PASS,
+      host: process.env.DB_HOST,
+      database: process.env.DB_DATABASE,
+      password: process.env.DB_PASS,
       // ssl: { rejectUnauthorized: false },
     },
   })
